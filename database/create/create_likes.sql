6d8e5bb1d9f623ffded203c01e0ae858099edc6f
Derek Cormier <derek@derekcormier.com>
2014-08-29 22:46:57 -0400

    fix - Add user_id to the likes table
    https://trello.com/c/wVR88pIp/19-create-user-likes-database-table
    
    Forgot to add the user_id to the likes table
    
    - Create row in likes for the user_id

--- database/create/create_likes.sql
+++ database/create/create_likes.sql
@@ -2,6 +2,7 @@ USE instaphoto_db;
 
 CREATE TABLE likes (
 	like_id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
+	user_id INT NOT NULL,
 	liked_photo_id VARCHAR(40) NOT NULL,
 	user_notified BOOLEAN NOT NULL,
 	PRIMARY KEY( like_id )
